1ae0da25fa22eaa224075b258903c607176d181c
Yanik Kendler <70104756+elYanuki@users.noreply.github.com>
2025-05-09 23:59:15 +0200

    idk?

--- backend/src/main/java/me/kendler/yanik/dto/scene/SceneSelectAttributeCreateDTO.java
+++
@@ -1,6 +0,0 @@
-package me.kendler.yanik.dto.scene;
-
-public record SceneSelectAttributeCreateDTO(
-    Long selectAttributeId,
-    String name
-) { }
---
+++ backend/src/main/java/me/kendler/yanik/dto/scene/SceneSelectAttributeOptionCreateDTO.java
@@ -0,0 +1,6 @@
+package me.kendler.yanik.dto.scene;
+
+public record SceneSelectAttributeOptionCreateDTO(
+    Long attributeDefinitionId,
+    String name
+) { }
